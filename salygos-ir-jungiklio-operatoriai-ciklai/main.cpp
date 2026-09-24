e7661b39fe8e3eb48e38e3bec80990992480aa63
Huong Tra Quan <s067041@ad.viko.lt>
2026-09-24 11:09:11 +0300

    added patikrinimas neigiamas ir teigiamas vidurkis pazymis uzduotis

---
+++ salygos-ir-jungiklio-operatoriai-ciklai/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include <iomanip>
+
+/* 1 uzduotis ir 2 uzuodtis
+    int main() {
+
+        const int studentGradesNum = 5;
+        int grade;
+        int highestGrade = 0;
+
+        for (int i = 1; i < studentGradesNum; i++) {
+            cout << "Iveskite " << i << " studento pazymi" << endl;
+            cin >> grade;
+            sum += grade;
+        }
+        double averageGrade = static_cast<double>(sum) / studentGradesNum;
+
+        if (grade >= 5) {
+            cout << "Vidurkis pazymis yra teigiama " << averageGrade << endl;
+        } else {
+            cout << "Vidurkis pazymis yra neigiama " << averageGrade << endl;
+        }
+
+        return 0;
+    } */
+
